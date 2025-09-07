90a51535fe386090f104a6bad698d8d8beb34d1c
eipna <serbispace@proton.me>
2025-09-08 01:24:28 +0800

    Created user repository class and database util class

---
+++ Saleling.Util/DatabaseUtil.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+
+namespace Saleling.Util
+{
+    public class DatabaseUtil
+    {
+        private static readonly string CONNECTION_STRING_NAME = "SalelingDB";
+
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings[CONNECTION_STRING_NAME].ConnectionString;
+        }
+    }
+}
