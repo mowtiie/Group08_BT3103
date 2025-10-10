fed286daae7fc51aa6c2743d6eafc4b571597853
Tutsy <markkerwinballelos@gmail.com>
2025-10-10 17:26:21 +0800

    Created GetTotalStockValue Procedure

---
+++ Saleling.DB/StoredProcedures/GetTotalStockValue.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE GetTotalStockValue
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT
+        ISNULL(SUM(PV.UnitsInStock * P.UnitPrice), 0.00) AS TotalStockValue
+    FROM
+        ProductVariants PV
+    INNER JOIN
+        Products P ON PV.ProductID = P.ProductID;
+END
+GO
