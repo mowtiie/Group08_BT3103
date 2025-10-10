27b63930fa6295c7b87cb9b950ef1ad7dae2f950
villalobosjamescarl30 <villalobosjamescarl30@gmail.com>
2025-10-10 16:36:59 +0800

    Creates Product Table

---
+++ Saleling.DB/Tables/Products.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+	[ProductID] INT PRIMARY KEY IDENTITY(1,1),
+    [ProductName] VARCHAR(50) NOT NULL,
+    [CategoryID] INT NOT NULL,
+    [SupplierID] INT,
+    [UnitPrice] DECIMAL(10, 2) NOT NULL,
+    [Description] NVARCHAR(MAX),
+    
+    FOREIGN KEY (CategoryID) REFERENCES Categories(CategoryID),
+    FOREIGN KEY (SupplierID) REFERENCES Suppliers(SupplierID)
+)
