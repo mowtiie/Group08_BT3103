de7f642d89e488b82acf50904f3a7e2285c4dfc4
eipna <serbispace@proton.me>
2025-10-16 22:04:09 +0800

    Removed un-needed load event for login form

---
+++ Saleling.UI/LoginForm.cs
@@ -0,0 +1,21 @@
+﻿using Saleling.Controller;
+
+namespace Saleling.UI
+{
+    public partial class LoginForm : Form
+    {
+
+        private readonly UserController accountController;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+            this.accountController = new UserController();
+        }
+
+        private void pbClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
