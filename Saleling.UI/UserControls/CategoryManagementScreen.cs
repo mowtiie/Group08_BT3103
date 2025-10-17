6630eeb5ef03cb3aae34a738ef5c1ad97b072278
eipna <serbispace@proton.me>
2025-10-17 21:23:05 +0800

    Created category management screen

---
+++ Saleling.UI/UserControls/CategoryManagementScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saleling.UI.UserControls
+{
+    public partial class CategoryManagementScreen : UserControl
+    {
+        public CategoryManagementScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
