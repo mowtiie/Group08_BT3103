8b4f7df607777bfa13d63ea258c8ddbce69480e8
eipna <serbispace@proton.me>
2025-10-17 09:51:11 +0800

    Created security util for hashing password using BCrypt

---
+++ Saleling.Util/SecurityUtil.cs
@@ -0,0 +1,17 @@
+﻿namespace Saleling.Util
+{
+    public class SecurityUtil
+    {
+        private const int DEFAULT_WORK_FACTOR = 12;
+
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, DEFAULT_WORK_FACTOR);
+        }
+
+        public static bool VerifyPassword(string plainTextPassword, string storedHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(plainTextPassword, storedHash);
+        }
+    }
+}
