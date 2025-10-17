4aa5f4ec6c778eb9e32d4fee8ea3b5899a284abb
eipna <serbispace@proton.me>
2025-10-17 09:59:40 +0800

    Updated login form logic

--- Saleling.UI/LogInForm.Designer.cs
+++ Saleling.UI/LogInForm.Designer.cs
@@ -181,6 +181,7 @@
             chkboxShowPassword.TabIndex = 4;
             chkboxShowPassword.Text = "Show Password";
             chkboxShowPassword.UseVisualStyleBackColor = true;
+            chkboxShowPassword.CheckedChanged += chkboxShowPassword_CheckedChanged;
             // 
             // btnLogin
             // 
@@ -194,6 +195,7 @@
             btnLogin.TabIndex = 5;
             btnLogin.Text = "LOGIN";
             btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
             // 
             // pbClose
             // 
--- Saleling.UI/LoginForm.cs
+++ Saleling.UI/LoginForm.cs
@@ -1,21 +1,72 @@
 ﻿using Saleling.Controller;
+using Saleling.Model;
+using Saleling.Util;
 
 namespace Saleling.UI
 {
     public partial class LoginForm : Form
     {
 
-        private readonly UserController accountController;
+        private readonly UserController userRepository;
 
         public LoginForm()
         {
             InitializeComponent();
-            this.accountController = new UserController();
+            this.userRepository = new UserController();
         }
 
         private void pbClose_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void chkboxShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = chkboxShowPassword.Checked;
+        }
+
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+            string usernameInput = txtUsername.Text.Trim();
+            string passwordInput = txtPassword.Text.Trim();
+
+            try
+            {
+                UserModel? validatedUser = await userRepository.AuthenticateUserAsync(usernameInput, passwordInput);
+                SessionUtil.Instance.SetLoggedInUser(validatedUser);
+
+                txtUsername.Clear();
+                txtPassword.Clear();
+
+                switch (validatedUser.Role)
+                {
+                    case "Admin":
+                        using (AdminNavigationForm adminNavigationForm = new AdminNavigationForm())
+                        {
+                            this.Hide();
+                            if (adminNavigationForm.ShowDialog() == DialogResult.OK)
+                            {
+                                this.Show();
+                            }
+                        }
+                        break;
+                    case "Cashier":
+                        using (CashierNavigationForm cashierNavigationForm = new CashierNavigationForm())
+                        {
+                            this.Hide();
+                            if (cashierNavigationForm.ShowDialog() == DialogResult.OK)
+                            {
+                                this.Show();
+                            }
+                        }
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                await LoggerUtil.Instance.LogAsync($"Error: {ex.Message}");
+                MessageBox.Show(ex.Message, "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
