559a782d5d903fb98146326ddd56bb0786f81c62
villalobosjames30 <villalobosjames30@gmail.com>
2025-11-28 22:59:03 +0800

    Created customer model class

---
+++ Saleling.Model/CustomerModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Saleling.Model
+{
+    public class CustomerModel
+    {
+        public int CustomerID { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public int? LoyaltyPoints { get; set; }
+    }
+}
