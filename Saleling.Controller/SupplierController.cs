ac8e96359d1c3b1a498980b5767baee75a8c8929
Adrienne20894 <jaloconadrienne@gmail.com>
2025-11-28 23:50:32 +0800

    Created controller and repository classes for supplier

---
+++ Saleling.Controller/SupplierController.cs
@@ -0,0 +1,17 @@
+﻿using Saleling.Repository;
+
+namespace Saleling.Controller
+{
+    public class SupplierController
+    {
+        private readonly SupplierRepository _supplierRepository;
+        public SupplierController()
+        {
+            _supplierRepository = new SupplierRepository();
+        }
+        public async Task<List<string>> GetAllNamesAsync()
+        {
+            return await _supplierRepository.GetAllNamesAsync();
+        }
+    }
+}
