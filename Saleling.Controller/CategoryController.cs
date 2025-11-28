3c721112bf387ce63cdb5a382004f34ffa0a3128
Adrienne20894 <jaloconadrienne@gmail.com>
2025-11-28 23:50:14 +0800

    Created repository and controller classes for category

---
+++ Saleling.Controller/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Saleling.Repository;
+
+namespace Saleling.Controller
+{
+    public class CategoryController
+    {
+        private readonly CategoryRepository _categoryRepository;
+
+        public CategoryController()
+        {
+            _categoryRepository = new CategoryRepository();
+        }
+
+        public async Task<List<string>> GetAllNamesAsync()
+        {
+            return await _categoryRepository.GetAllNamesAsync();
+        }
+
+        public async Task<int> GetTotalCountAsync()
+        {
+            return await _categoryRepository.GetTotalCountAsync();
+        }
+    }
+}
