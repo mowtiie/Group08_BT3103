4c405c70b7f7e34f29f399e7fdfe4762d4190352
villalobosjames30 <villalobosjames30@gmail.com>
2025-11-28 22:47:12 +0800

    Created sale cart model class

---
+++ Saleling.Model/Sale/SaleCartModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Saleling.Model.Sale
+{
+    public class SaleCartModel
+    {
+        public int VariantID { get; set; }
+        public required string ProductName { get; set; }
+        public required string SKU { get; set; }
+        public required string Size { get; set; }
+        public required string Color { get; set; }
+        public int CurrentStock { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; }
+        public decimal LineTotal { get; set; }
+    }
+}
