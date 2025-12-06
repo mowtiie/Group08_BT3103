99d39cd5d6dfec61f3f0f729c51aa67a56dcaf85
mowtiie <serbispace@proton.me>
2025-12-06 22:45:06 +0800

    Added culture info for ph

--- Saleling.UI/Program.cs
+++ Saleling.UI/Program.cs
@@ -1,15 +1,16 @@
+using System.Globalization;
+
 namespace Saleling.UI
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            CultureInfo phCulture = new CultureInfo("en-PH");
+            CultureInfo.DefaultThreadCurrentCulture = phCulture;
+            CultureInfo.DefaultThreadCurrentUICulture = phCulture;
+
             ApplicationConfiguration.Initialize();
             Application.Run(new LoginForm());
         }
